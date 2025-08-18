1f91c2b2e7856a9e5ddba5e3f19dfceca4dab015
Paul Miller <paul@paulmillr.com>
2025-08-18 12:28:58 +0000

    Merge devdeps: should, bmark into jsbt

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,4 +1,4 @@
-import { describe, should } from 'micro-should';
+import { describe, should } from '@paulmillr/jsbt/test.js';
 import { deepStrictEqual, rejects } from 'node:assert';
 import { Buffer } from 'node:buffer';
 import { createServer } from 'node:http';
